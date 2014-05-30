1e205fb9f8595b766db940c7469c57f4180f1153
Steve Polyak <stevepolyak@mac.com>
2014-05-29 20:02:54 -0500

    update express 4.x

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var gzippo = require('gzippo');
 var express = require('express');
-var logger = require('logger');
+var morgan = require('morgan');
 var app = express();
  
-app.use(express.logger('dev'));
+app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
